468398a4605cf2e1b7031229b2a910a5f0934321
Walmir <owalmirneto@gmail.com>
2025-10-23 16:44:14 -0300

    Move constants to a separeted file

---
+++ src/data.rb
@@ -0,0 +1,20 @@
+STUDENTS = [
+  { name: "Alice", score: 95 },
+  { name: "Bob", score: 58 },
+  { name: "Charlie", score: 72 },
+  { name: "Daisy", score: 88 },
+  { name: "Eve", score: 40 },
+  { name: "Frank", score: 65 },
+  { name: "Grace", score: 81 },
+  { name: "Hank", score: 90 },
+  { name: "Ivy", score: 100 },
+  { name: "Jack", score: 54 }
+]
+
+RANGE_GRADE_RULES = {
+  A: 90..100,
+  B: 80..89,
+  C: 70..79,
+  D: 60..69,
+  F: 0..59
+}
--- src/students.rb
+++ src/students.rb
@@ -1,29 +1,10 @@
+require_relative "data"
+
 class Students
-  LIST = [
-    { name: "Alice", score: 95 },
-    { name: "Bob", score: 58 },
-    { name: "Charlie", score: 72 },
-    { name: "Daisy", score: 88 },
-    { name: "Eve", score: 40 },
-    { name: "Frank", score: 65 },
-    { name: "Grace", score: 81 },
-    { name: "Hank", score: 90 },
-    { name: "Ivy", score: 100 },
-    { name: "Jack", score: 54 }
-  ]
-
-  RANGE_GRADES = {
-    A: 90..100,
-    B: 80..89,
-    C: 70..79,
-    D: 60..69,
-    F: 0..59
-  }
-
   def self.total_score
     output = 0
 
-    Students::LIST.map do |student|
+    STUDENTS.map do |student|
       output = output + student[:score]
     end
 
@@ -31,15 +12,15 @@ class Students
   end
 
   def self.average
-    Students.total_score / Students::LIST.size.to_f
+    Students.total_score / STUDENTS.size.to_f
   end
 
   def self.grades
     output = {}
 
-    Students::LIST.map do |student|
-      Students::RANGE_GRADES.keys.map do |grade|
-        if Students::RANGE_GRADES[grade].to_a.include?(student[:score])
+    STUDENTS.map do |student|
+      RANGE_GRADE_RULES.keys.map do |grade|
+        if RANGE_GRADE_RULES[grade].to_a.include?(student[:score])
           output[grade] = output[grade] ? output[grade] + 1 : 1
         end
       end
@@ -60,7 +41,7 @@ class Students
     @grouped_students ||= begin
       @grouped = {}
 
-      Students::LIST.map do |student|
+      STUDENTS.map do |student|
       @grouped[student[:score]] = student[:name]
       end
 
